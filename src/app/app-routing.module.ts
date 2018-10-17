7baac81f44ea4788a2f1afc76bc842167688a7d7
Muhammad Irfan <irfan.abrazak@gmail.com>
2018-10-17 10:04:14 +0800

    Refactored front-end dependencies with bower.
    Front-end dependencies originally executed from npm now leverages bower. Moving forward all front-end dependencies (plugins) will shall be saved in bower.json. Plugins downloaded are not tracked.
    
    Font paths (glyphicons and font-aweseom) are defined to import fonts from bootstrap-sass & fontawesome plugins.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 import { DetailsComponent } from './details/details.component';
 import { DataComponent } from './data/data.component';
--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 declare var $: any;
+declare const google: any;
+
 
 @Component({
   selector: 'app-maps',
@@ -10,7 +13,17 @@ declare var $: any;
 
 export class MapsComponent implements OnInit {
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
   ngOnInit() {
+
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
 
   }
 
